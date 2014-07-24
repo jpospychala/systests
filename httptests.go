c14e1d29373eeafb20ce3546fabfaf450566917a
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-07-24 23:31:59 +0200

    reformat with go fmt

--- httptests.go
+++ httptests.go
@@ -1,57 +1,57 @@
 package httptests
 
 import (
-  "errors"
-  "strings"
-  "io/ioutil"
-  "net/http"
-  "time"
-  )
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
 
 type Timing struct {
-  latency time.Duration
-  responseTime time.Duration
+	latency      time.Duration
+	responseTime time.Duration
 }
 
 func GetHttpUrl(url string) (string, Timing, error) {
-  start := time.Now()
-  resp, err := http.Get(url)
-  if err != nil {
-    return "", Timing{}, err
-  }
-  latency := time.Now().Sub(start)
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", Timing{}, err
+	}
+	latency := time.Now().Sub(start)
 
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
-  responseTime := time.Now().Sub(start)
-  if err != nil {
-    return "", Timing{}, err
-  }
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	responseTime := time.Now().Sub(start)
+	if err != nil {
+		return "", Timing{}, err
+	}
 
-  return string(body), Timing{latency, responseTime}, nil
+	return string(body), Timing{latency, responseTime}, nil
 }
 
-func ExpectHttpUrl(url string, expectedBody string) (error) {
-  body, _, err := GetHttpUrl(url)
-  if err != nil {
-    return err
-  }
+func ExpectHttpUrl(url string, expectedBody string) error {
+	body, _, err := GetHttpUrl(url)
+	if err != nil {
+		return err
+	}
 
-  if ! strings.Contains(body, expectedBody) {
-    return errors.New("Invalid response")
-  }
+	if !strings.Contains(body, expectedBody) {
+		return errors.New("Invalid response")
+	}
 
-  return nil
+	return nil
 }
 
-func WaitForUrl(url string, timeout time.Duration) (error) {
-  absTimeout := time.Now().Add(timeout)
+func WaitForUrl(url string, timeout time.Duration) error {
+	absTimeout := time.Now().Add(timeout)
 
-  _, _, err := GetHttpUrl(url)
-  for err != nil && time.Now().Before(absTimeout) {
-    time.Sleep(50 * time.Millisecond)
-    _, _, err = GetHttpUrl(url)
-  }
+	_, _, err := GetHttpUrl(url)
+	for err != nil && time.Now().Before(absTimeout) {
+		time.Sleep(50 * time.Millisecond)
+		_, _, err = GetHttpUrl(url)
+	}
 
-  return err
+	return err
 }
